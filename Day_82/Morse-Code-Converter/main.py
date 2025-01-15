8c0774a4132d1ffabe182ec2be14171b162297e3
pdob <75912982+pdob-git@users.noreply.github.com>
2025-01-15 16:22:54 +0100

    main. Day_82. Add decode
    Added functions to decode from Morse Code
    
    changes:
    	modified:   Day_82/Morse-Code-Converter/main.py

--- Day_82/Morse-Code-Converter/main.py
+++ Day_82/Morse-Code-Converter/main.py
@@ -26,41 +26,110 @@ def load_json(path: str) -> dict[str, str]:
 
 def encode_to_morse(string: str, conversion_dict: dict[str, str]):
     """Encodes strings in Morse Code
+    Display results
 
     :param string: string to be encoded
     :param conversion_dict: dictionary with conversions
     """
 
     print(f"Source string:\n{string}")
+
     try:
-        morse_list = [encoder(char, conversion_dict) for char in string.upper()]
-        # print(f"Morse result list: {morse_list}")
-        morse_string = "".join(morse_list)
+        morse_string = encode(conversion_dict, string)
         print(f"Morse result:\n{morse_string}")
+        print()  # empty line to separate results
     except KeyError as err:
         error_msg = str(err)
         sys.exit(f"Error occurred:  Char {error_msg} is not supported.")
 
 
+def encode(conversion_dict: dict[str, str], string: str) -> str:
+    """Sub method for `encode_to_morse`
+    Encodes strings in Morse Code
+
+    :param conversion_dict:  dictionary with conversions
+    :param string: string to be encoded
+    :return: result of encoded string
+    """
+
+    encoded_list = ["{} ".format(encoder(char, conversion_dict)) for char in string.upper()]
+
+    encoded_string = "".join(encoded_list)
+    encoded_string = encoded_string.replace("\n ", "\n")
+    return encoded_string
+
+
+
+def decode(conversion_dict: dict[str, str], string: str) -> str:
+    """Sub method for `decode_from_morse`
+    Decodes strings from Morse Code
+
+    :param conversion_dict:  dictionary with conversions
+    :param string: string to be decoded
+    :return: result of decoded string
+    """
+
+    # Add space ad the beginning of each line and preserve end of lines in list
+    string = string.replace("\n","\n ")
+
+    morse_codes_list = string.split(" ")
+    morse_codes_no_empty = [char for char in morse_codes_list if char != '']
+
+    encoded_list = [encoder(char, conversion_dict) for char in morse_codes_no_empty]
+    encoded_string = "".join(encoded_list)
+    return encoded_string
+
+
+
 def encoder(char: str, conversion_dict: dict[str, str]) -> str:
-    """Encodes char in Morse Code
+    """Encodes char in Morse Code if provided conversion dictionary is in form
+    {Sign: Morse_code}
+    Decodes char from morse code if provided conversion dictionary is in form
+    {Morse_code: Sign}
 
     :param char: char to encode
     :param conversion_dict: dictionary with conversions
     :raises: KeyError with not supported character
-    :return: converted Morse code
+    :return: single character converted Morse code or Decoded from Morse Code
     """
 
     if char == "\n":
         return char
-    return "{} ".format(conversion_dict[char])
+    return conversion_dict[char]
+
+
+def decode_from_morse(morse_string: str, conversion_dict: dict[str, str]):
+    """Decodes strings from Morse Code
+    Display results
+
+    :param morse_string: string to be decoded
+    :param conversion_dict: dictionary with conversions
+    """
+
+    inverted_dict = {value: key for key, value in conversion_dict.items()}
+    print(f"Morse string:\n{morse_string}")
+
+    try:
+        result_string = decode(inverted_dict, morse_string)
+        print(f"Decoded result:\n{result_string}")
+        print() #empty line to separate results
+    except KeyError as err:
+        error_msg = str(err)
+        sys.exit(f"Error occurred:  Char {error_msg} is not supported.")
 
 
 def main():
     loaded_dict = load_json(MORSE_CODE_JSON)
     # print(loaded_dict)
     encode_to_morse("Mama ma kota\nSOS\nYEAH!", loaded_dict)
-    encode_to_morse("An exception has occurred!~", loaded_dict)
+    encode_to_morse("The joke was great!", loaded_dict)
+    morse_one = r"""-- •- -- •- / -- •- / -•- --- - •- 
+••• --- ••• 
+-•-- • •- •••• -•-•-- """
+    morse_two = r"""- •••• • / •--- --- -•- • / •-- •- ••• / --• •-• • •- - -•-•-- ~"""
+
+    decode_from_morse(morse_one, loaded_dict)
+    decode_from_morse(morse_two, loaded_dict)
 
 
 if __name__ == '__main__':
